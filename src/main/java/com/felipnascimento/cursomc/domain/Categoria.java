13c6d1ad7504dc695995e99c398d3b0baf3b851a
Felipe <felipe1.jn@hotmail.com>
2018-06-27 15:31:50 -0300

    config bd h2 and create bd h2

--- src/main/java/com/felipnascimento/cursomc/domain/Categoria.java
+++ src/main/java/com/felipnascimento/cursomc/domain/Categoria.java
@@ -2,7 +2,15 @@ package com.felipnascimento.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
 public class Categoria implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private static final long serialVersionUID = 1L;
